78ee2f79bf8f3a7b42ba654bc17eac940ae73eb6
wesandradealves <wesandradealves@gmail.com>
2018-10-11 06:55:18 -0300

    - Home

---
+++ assets/css/components/combobox/combobox.js
@@ -0,0 +1,52 @@
+function combobox(){
+	if($('[combobox]').length)
+		var $this = $('[combobox]'),
+			$options = $this.children('ul'),
+			$handler = $this.children('a');
+
+		function toggle(){
+	    	$this.toggleClass('toggle');
+	    	$options.toggle();			
+		}  
+
+		function setVal(title, slug){
+			var $title = title,
+				$slug = slug;
+
+				$handler
+				.attr({ 'data-value' : $slug, 'title' : $title })
+				.children('span').text($title);
+		}
+
+		function selVal(){
+			$options.children().find('a').each(function() {
+				var $option = $(this);
+				$option.on('click', function(e) {
+			    	toggle();
+			    	
+			    	var $obj = new Object();
+
+			    	$obj['title'] = $option.text(),
+			    	$obj['slug'] = $option.attr('data-value');
+
+			    	setVal($obj['title'], $obj['slug']);
+			    });    
+			});
+		}
+
+	    $handler.on('click', function(e) {
+	    	toggle();
+	    });    
+
+		$(document).mouseup(function (e){
+		    var container = $this;
+		    if (!container.is(e.target) 
+		    && container.has(e.target).length === 0) 
+		    {
+		    	if($this.is('.toggle') && $options.is(':visible'))
+		    		toggle();
+		    }
+		});	    
+
+		selVal();
+}
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,19 @@
+var $magnifying_glass = document.getElementById('magnifying_glass'),
+	$magnifying_glass_inner = document.getElementById('magnifying_glass_inner');
+
+
+function closeMenu(){
+	$(".tcon-transform").removeClass("tcon-transform");
+}
+
+function _mobileNavigation(){
+	$(".tcon").toggleClass("tcon-transform");
+} 
+
+
 $(document).ready(function () {
+	combobox();
+	new Parallax($magnifying_glass);
 });
       
       
